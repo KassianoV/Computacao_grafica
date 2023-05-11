0051a8d1dd9874e23c6e10c253efc915d1438d29
KassianoV <kassianovieira@id.uff.br>
2023-05-10 21:15:19 -0300

    Finalizado Tarefa 09

--- Borboleta_circular.cpp
+++ Borboleta_circular.cpp
@@ -10,37 +10,33 @@ int main(){
     std::vector<unsigned int> indices = triangulate_polygon(P); // Triangulação para preenchimento de cor
 
     vec2 v = {400,300};
+    ImageRGB G(800, 600);
+    mat3 T = {
+		1.0, 0.0, v[0],
+		0.0, 1.0, v[1],
+		0.0, 0.0, 1.0
+	};
 
-    float t = 0;
-    for(float l=0; l<1;l+=0.084){
-        mat3 T = {
-            1.0, 0.0, v[0],
-            0.0, 1.0, v[1],
-            0.0, 0.0, 1.0 
-        };
+    mat3 Ti = {
+		1.0, 0.0, -v[0],
+		0.0, 1.0, -v[1],
+		0.0, 0.0, 1.0
+	};
 
-        mat3 Ti = {
-            1.0, 0.0, -v[0],
-            0.0, 1.0, -v[1],
-            0.0, 0.0, 1.0
-        };
+    for(int t=0;t<12;t++){
+        float r = 2*3.14/12*(t+1);
+	    mat3 R = {
+		    cos(r), -sin(r), 0.0,
+		    sin(r),  cos(r), 0.0,
+		    0.0,     0.0,    1.0
+	    };
 
-         mat3 R = {
-            cos(t), -sin(t), 0.0,
-            sin(t),  cos(t), 0.0,
-            0.0,     0.0, 1.0
-        };
-     Elements<Triangles> L{indices};
-       // TriLines<Elements<Triangles>> A{indices};   ////2 opção - Usando trilines para marcar a triangulação
-       
-        ImageRGB img(700,700);
-        img.fill(white);
-       // auto cor2 = lerp(l, purple, green); //2 opção - cores para a marcação da triangulação
-        img.draw((T*R*Ti)*P, L, cor);
-       // img.draw((T*R*Ti)*P, A, cor2); //2 opção - desenhando a marcação da triangulação
-        t+=0.525;
+        LineStrip L{P.size()};
+        
+        
+        G.fill(white);
+        render2d(P, L, red, G);
+        render2d((T*R*Ti)*P, L, blue, G);
     }
-	
-	img.savePNG("Borboleta_circle.png");
-
+    G.savePNG("Borboleta_circular.png");
 }
